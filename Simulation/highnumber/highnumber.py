bfffe8c09ea8c4a72b087f35e031cd3652dbf000
RkdJIHYE <kangbs210210@gmail.com>
2026-01-20 00:07:53 +0900

    Feat:큰수의 법칙 완성

--- Simulation/highnumber/highnumber.py
+++ Simulation/highnumber/highnumber.py
@@ -9,10 +9,40 @@
 import sys 
 
 input = sys.stdin.readline
-
-
 N,M,K = map(int,input().split())
+numbers = list(map(int,input().split()))
 
-print(N)
+#print(N)
+#print(numbers)
 
+#한번 정렬해주는 것이 필요
+numbers.sort()
 
+fir = numbers[-1] #가장 큰 수 
+sec = numbers[-2] #두번째로 큰 수 
+
+print(fir)
+print(sec)
+
+#M은 반복 횟수
+#k는 최대 반복 횟수
+
+#결과값 저장할 변수 result
+
+result = 0
+
+while True:
+    for i in range(K):
+        if M == 0:
+            break
+        result+=fir
+        M-=1
+    if M == 0:
+        break
+    
+    result+=sec
+    M-=1
+
+print(result)
+    
+    
